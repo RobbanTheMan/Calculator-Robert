349f17eaf884525957ba01ce2552a7871542282e
RobbanTheMan <robban-p@hotmail.com>
2016-04-06 15:22:34 +0200

    I have now added and created subtraction, division and multiplication

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -1,12 +1,27 @@
 
 public class calculator {
 
-	public double addition(double first, double secound){
+	public double add(double first, double secound){
 		double addition;
 		addition = first + secound;
 		return addition;
 	}
 	
+	public double sub(double first, double secound){
+		double sub;
+		sub = first - secound;
+		return sub;
+	}
 	
+	public double div(double first, double secound){
+		double div;
+		div = first / secound;
+		return div;
+	}
+	public double multi(double first, double secound){
+		double multi;
+		multi = first * secound;
+		return multi;
 	
+}
 }
