049c7cc0b28314c41d8dc8b279c8c45ebb2d6b6c
RobbanTheMan <robban-p@hotmail.com>
2016-04-06 16:07:07 +0200

    I have created a test case for method addition and tested three different tests.

--- src/test/java/TestCalculationAdd.java
+++ src/test/java/TestCalculationAdd.java
@@ -1,17 +1,33 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestCalculationAdd {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	static calculator calculator;
+	
+	@Before
+	public void setUp() throws Exception {
+	
+		calculator = new calculator();
+		
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPositive() {
+		assertEquals(10,calculator.add(5, 5), 0d);
+		
 	}
-
+	@Test
+	public void testNegative() {
+		assertEquals(-10,calculator.add(-5, -5), 0d);
+		
+	}
+	@Test
+	public void testDecimal() {
+		assertEquals(11,calculator.add(5.5, 5.5), 0d);
+	}
 }
+
