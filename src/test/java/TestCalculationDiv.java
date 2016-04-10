6e396586b28cd5020aff13657f8ed9fa4cf04b03
RobbanTheMan <robban-p@hotmail.com>
2016-04-10 14:59:21 +0200

    made three new test to the method div. hade trouble to calculate -8 / -8 but i figuared it out.

--- src/test/java/TestCalculationDiv.java
+++ src/test/java/TestCalculationDiv.java
@@ -1,17 +1,32 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestCalculationDiv {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+static calculator calculator;
+	
+	@Before
+	public void setUp() throws Exception {
+	
+		calculator = new calculator();
+		
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPositive() {
+		assertEquals("Det förväntade resultatet ska bli:2", 2 ,calculator.div(6, 3), 0d);
+		
 	}
-
+	@Test
+	public void testNegative() {
+		assertEquals("Det förväntade resultatet ska bli:1",1,calculator.div(-8, -8), 0d);
+		
+	}
+	@Test
+	public void testDecimal() {
+		assertEquals("Det förväntade resultatet ska bli:1.9",2,calculator.div(10.5, 5.25), 0d);
+	}
 }
