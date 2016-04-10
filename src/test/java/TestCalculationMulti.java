7f926ce2bb0e370879ec7aa24b6238c42b5cb585
RobbanTheMan <robban-p@hotmail.com>
2016-04-10 15:06:20 +0200

    the last three tests are now finished for method multi. I tried with more decimals this time.

--- src/test/java/TestCalculationMulti.java
+++ src/test/java/TestCalculationMulti.java
@@ -1,17 +1,32 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestCalculationMulti {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+static calculator calculator;
+	
+	@Before
+	public void setUp() throws Exception {
+	
+		calculator = new calculator();
+		
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPositive() {
+		assertEquals("Det förväntade resultatet ska bli:81", 81 ,calculator.multi(9, 9), 0d);
+		
 	}
-
+	@Test
+	public void testNegative() {
+		assertEquals("Det förväntade resultatet ska bli:30",30,calculator.multi(-6, -5), 0d);
+		
+	}
+	@Test
+	public void testDecimal() {
+		assertEquals("Det förväntade resultatet ska bli:6.5",6.51525,calculator.multi(2.55, 2.555), 0d);
+	}
 }
